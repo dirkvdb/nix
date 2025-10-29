0a5af691030983d1ae6bccd4abc666e2824909ff
Dirk Vanden Boer <dirk.vdb@gmail.com>
2025-10-29 19:16:44 +0100

    Modularization

---
+++ modules/nixos/system/boot/systemd/default.nix
@@ -0,0 +1,59 @@
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
+let
+  cfg = config.local.system.boot.systemd;
+in
+{
+  options.local.system.boot.systemd = {
+    enable = lib.mkEnableOption "Enable systemd bootloader";
+    graphical = lib.mkEnableOption "Enable graphical boot without kernel messages";
+  };
+
+  config = lib.mkIf cfg.enable (
+    lib.mkMerge [
+      {
+        boot.loader.systemd-boot.enable = true;
+        boot.loader.systemd-boot.consoleMode = "auto";
+      }
+
+      (lib.mkIf cfg.graphical {
+
+        fonts = {
+          packages = [
+            pkgs.noto-fonts
+          ];
+        };
+
+        environment.systemPackages = with pkgs; [
+          nixos-icons
+        ];
+
+        boot.consoleLogLevel = 3;
+        boot.initrd.verbose = false;
+        boot.initrd.systemd.enable = true;
+        boot.kernelParams = [
+          "quiet"
+          "splash"
+          "intremap=on"
+          "boot.shell_on_fail"
+          "udev.log_priority=3"
+          "rd.systemd.show_status=auto"
+          "vt.global_cursor_default=0"
+          "fbcon=nodefer"
+          "video=efifb:nobgrt" # disable firmware vendor logo
+          "amdgpu.modeset=1"
+        ];
+
+        # plymouth, showing after LUKS unlock
+        boot.plymouth.enable = true;
+        boot.plymouth.theme = "spinner";
+        boot.plymouth.font = "${pkgs.noto-fonts}/share/fonts/noto/NotoSans[wdth,wght].ttf";
+        boot.plymouth.logo = "${pkgs.nixos-icons}/share/icons/hicolor/128x128/apps/nix-snowflake.png";
+      })
+    ]
+  );
+}
