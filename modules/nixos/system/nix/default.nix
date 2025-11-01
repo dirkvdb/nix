a1c710fca93230e3e2f027fc6e8f9842fbe724eb
Dirk Vanden Boer <dirk.vdb@gmail.com>
2025-11-01 22:40:14 +0100

    Move Nix GC options to garbage-collect module

--- modules/nixos/system/nix/default.nix
+++ modules/nixos/system/nix/default.nix
@@ -1,14 +1,8 @@
 {
-  lib,
   ...
 }:
 {
   nix = {
-    gc = {
-      automatic = lib.mkDefault true;
-      options = lib.mkDefault "--delete-older-than 1w";
-    };
-
     optimise.automatic = true;
   };
 }
