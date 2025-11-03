8a2c1a1ad12d35243c4ec6970fabae8f8ff86775
Dirk Vanden Boer <dirk.vdb@gmail.com>
2025-11-03 17:19:56 +0100

    Enable skhd hotkey manager with custom keybindings

---
+++ modules/home/apps/skhd/default.nix
@@ -0,0 +1,29 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+let
+  inherit (config.local) user;
+  cfg = config.local.home-manager.skhd;
+in
+{
+  options.local.home-manager.skhd = {
+    enable = lib.mkEnableOption "Hotkey manager";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home-manager.users.${user.name} = {
+      services.skhd = {
+        enable = true;
+        config = ''
+          alt - return : ${pkgs.wezterm}/bin/wezterm
+          alt - d : ${pkgs.zed-editor}/bin/zeditor
+          alt - b : open -na zen
+          alt + shift - w : open -na whatsapp
+        '';
+      };
+    };
+  };
+}
