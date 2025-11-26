d59d3ca54a19723707fd8f49cba6ad2e930e5691
Dirk Vanden Boer <dirk.vdb@gmail.com>
2025-11-26 23:05:04 +0100

    Update nixpkgs input to nixos-25.11 branch

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "nix system flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
 
