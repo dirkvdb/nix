33eb6ad26dee769eac4d6002612db65636ee3200
Dirk Vanden Boer <dirk.vdb@gmail.com>
2026-01-05 10:17:41 +0100

    Enable Vulkan and Wayland support in Wine for binfmt

--- modules/nixos/system/binfmt/default.nix
+++ modules/nixos/system/binfmt/default.nix
@@ -7,6 +7,13 @@
 let
   cfg = config.local.system.binfmt;
   isArmLinux = pkgs.stdenv.hostPlatform.isAarch64;
+
+  # Wine with Vulkan and Wayland support enabled
+  # Use stagingFull which includes all necessary support libraries
+  wineWithVulkan = pkgs.wineWowPackages.stagingFull.override {
+    vulkanSupport = true;
+    waylandSupport = true;
+  };
 in
 {
   options.local.system.binfmt = {
@@ -23,5 +30,12 @@ in
     ++ lib.optionals isArmLinux [
       "x86_64-linux"
     ];
+
+    # Override wine package for Windows executables to include Vulkan and Wayland support
+    boot.binfmt.registrations = lib.mkIf (!isArmLinux) {
+      x86_64-windows = {
+        interpreter = "${wineWithVulkan}/bin/wine";
+      };
+    };
   };
 }
