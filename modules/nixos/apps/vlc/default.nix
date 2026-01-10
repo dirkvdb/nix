89f47c996a7a2538c1b7a72e951b7a24f7c40c9d
Dirk Vanden Boer <dirk.vdb@gmail.com>
2026-01-10 21:38:10 +0100

    Wrap VLC to set QT_SCALE_FACTOR and fix desktop entry

--- modules/nixos/apps/vlc/default.nix
+++ modules/nixos/apps/vlc/default.nix
@@ -6,6 +6,30 @@
 }:
 let
   cfg = config.local.apps.vlc;
+  vlcWrapped = pkgs.symlinkJoin {
+    name = "vlc-wrapped";
+    paths = [
+      pkgs.vlc
+    ];
+
+    buildInputs = [
+      pkgs.makeWrapper
+    ];
+
+    postBuild = ''
+      wrapProgram "$out/bin/vlc" --set QT_SCALE_FACTOR 2.0
+
+      if [ -f "$out/share/applications/vlc.desktop" ]; then
+        if [ -L "$out/share/applications/vlc.desktop" ]; then
+          rm "$out/share/applications/vlc.desktop"
+          cp "${pkgs.vlc}/share/applications/vlc.desktop" "$out/share/applications/vlc.desktop"
+          chmod u+w "$out/share/applications/vlc.desktop"
+        fi
+        sed -i "s|^Exec=[^ ]*|Exec=$out/bin/vlc|" "$out/share/applications/vlc.desktop"
+        sed -i "s|^TryExec=.*|TryExec=$out/bin/vlc|" "$out/share/applications/vlc.desktop"
+      fi
+    '';
+  };
 in
 {
   options.local.apps.vlc = {
@@ -14,7 +38,7 @@ in
 
   config = lib.mkIf cfg.enable {
     environment.systemPackages = [
-      pkgs.vlc
+      vlcWrapped
     ];
   };
 }
