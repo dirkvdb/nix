1fbc44776ac3cfed46f7f9536af42382ed79487a
Dirk Vanden Boer <dirk.vdb@gmail.com>
2026-01-27 07:53:30 +0100

    Change scratch space keybind

--- modules/home/apps/hyprland/bindings.nix
+++ modules/home/apps/hyprland/bindings.nix
@@ -57,7 +57,7 @@ in
       ]
       ++ [
         "$mod ALT, SPACE, Menu, exec, nixcfg-menu"
-        "ALT, X, Special workspace, togglespecialworkspace"
+        "$mod, X, Special workspace, togglespecialworkspace"
 
         "$mod, ESCAPE, Power menu, exec, nixcfg-menu system"
         "$mod, RETURN, Terminal, exec, $terminal --working-directory=\"$(nixcfg-cmd-terminal-cwd)\""
