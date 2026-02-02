82324b8cd34f1d80e6f7a9e23153f895f6dc2784
Dirk Vanden Boer <dirk.vdb@gmail.com>
2026-02-02 23:17:19 +0100

    nix tweaks

--- modules/nixos/system/nix/nh/default.nix
+++ modules/nixos/system/nix/nh/default.nix
@@ -19,5 +19,11 @@ in
       clean.extraArgs = "--keep-since 4d --keep 3";
       flake = "/home/${config.local.user.name}/nix"; # sets NH_FLAKE variable
     };
+
+    # Run programs without nix-shell
+    # Usage: $ , cowsay "Hello"
+    # It will install cowsay temporily, run the program in your shell, and then
+    # remove it from your PATH again afterwards.
+    programs.nix-index-database.comma.enable = lib.mkDefault true;
   };
 }
