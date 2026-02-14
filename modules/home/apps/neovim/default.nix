077477fbf9f590df5a979c92c3ea39d635c6fecc
Dirk Vanden Boer <dirk.vdb@gmail.com>
2026-02-14 21:50:39 +0100

    Remove neovim patch

--- modules/home/apps/neovim/default.nix
+++ modules/home/apps/neovim/default.nix
@@ -9,113 +9,6 @@ let
   inherit (config.local) user;
   cfg = config.local.apps.neovim;
   mkUserHome = mkHome user.name;
-  nvfTreesitterTextobjectsPatch =
-    { lib, config, ... }:
-    let
-      inherit (lib.nvim.dag) entryAfter;
-      inherit (lib.nvim.lua) toLuaObject;
-      ts = config.vim.treesitter;
-      tsTextobjects = ts.textobjects;
-    in
-    {
-      config = lib.mkIf (ts.enable && tsTextobjects.enable) {
-        # Patch nvf's textobjects loader to the current nvim-treesitter-textobjects API.
-        vim.pluginRC.treesitter-textobjects = lib.mkForce (
-          entryAfter [ "treesitter" ] ''
-            local cfg = ${toLuaObject tsTextobjects.setupOpts}
-
-            local textobjects = require("nvim-treesitter-textobjects")
-            local select = require("nvim-treesitter-textobjects.select")
-            local move = require("nvim-treesitter-textobjects.move")
-
-            local setup_cfg = vim.deepcopy(cfg)
-
-            if type(setup_cfg.select) == "table" then
-              setup_cfg.select.keymaps = nil
-            end
-
-            if type(setup_cfg.move) == "table" then
-              setup_cfg.move.goto_next_start = nil
-              setup_cfg.move.goto_next_end = nil
-              setup_cfg.move.goto_previous_start = nil
-              setup_cfg.move.goto_previous_end = nil
-              setup_cfg.move.goto_next = nil
-              setup_cfg.move.goto_previous = nil
-            end
-
-            textobjects.setup(setup_cfg)
-
-            local function normalize_mapping_spec(spec)
-              if spec == nil or spec == false then
-                return nil, nil, nil
-              end
-
-              if type(spec) == "string" then
-                return spec, "textobjects", nil
-              end
-
-              if type(spec) ~= "table" then
-                return nil, nil, nil
-              end
-
-              if vim.islist(spec) then
-                return spec, "textobjects", nil
-              end
-
-              if spec.query == nil then
-                return nil, nil, nil
-              end
-
-              return spec.query, spec.query_group or spec.queryGroup or spec.group or "textobjects", spec.desc
-            end
-
-            local function map_select(keymaps)
-              if type(keymaps) ~= "table" then
-                return
-              end
-
-              for lhs, spec in pairs(keymaps) do
-                local query, query_group, desc = normalize_mapping_spec(spec)
-                if query ~= nil then
-                  vim.keymap.set({ "x", "o" }, lhs, function()
-                    select.select_textobject(query, query_group)
-                  end, { silent = true, desc = desc })
-                end
-              end
-            end
-
-            local function map_move(keymaps, method)
-              if type(keymaps) ~= "table" then
-                return
-              end
-
-              for lhs, spec in pairs(keymaps) do
-                local query, query_group, desc = normalize_mapping_spec(spec)
-                if query ~= nil then
-                  vim.keymap.set({ "n", "x", "o" }, lhs, function()
-                    move[method](query, query_group)
-                  end, { silent = true, desc = desc })
-                end
-              end
-            end
-
-            if not (type(cfg.select) == "table" and cfg.select.enable == false) then
-              map_select(type(cfg.select) == "table" and cfg.select.keymaps or nil)
-            end
-
-            if not (type(cfg.move) == "table" and cfg.move.enable == false) then
-              local move_cfg = type(cfg.move) == "table" and cfg.move or {}
-              map_move(move_cfg.goto_next_start, "goto_next_start")
-              map_move(move_cfg.goto_next_end, "goto_next_end")
-              map_move(move_cfg.goto_previous_start, "goto_previous_start")
-              map_move(move_cfg.goto_previous_end, "goto_previous_end")
-              map_move(move_cfg.goto_next, "goto_next")
-              map_move(move_cfg.goto_previous, "goto_previous")
-            end
-          ''
-        );
-      };
-    };
 
   # Helper to create keymaps more concisely
   mkKeymap = mode: key: action: desc: {
@@ -135,8 +28,6 @@ in
       enable = true;
 
       settings = {
-        imports = [ nvfTreesitterTextobjectsPatch ];
-
         vim.viAlias = true;
         vim.vimAlias = true;
         vim.lsp = {
