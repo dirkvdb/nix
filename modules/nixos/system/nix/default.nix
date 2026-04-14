a3ef283c740e088e2bb1dd8a27afa7fb6566c404
Dirk Vanden Boer <dirk.vdb@gmail.com>
2026-04-14 09:30:34 +0200

    Add sops template for user nix.conf with GitHub token

--- modules/nixos/system/nix/default.nix
+++ modules/nixos/system/nix/default.nix
@@ -1,7 +1,14 @@
 {
   pkgs,
+  lib,
+  config,
   ...
 }:
+let
+  inherit (lib) mkIf;
+  inherit (config.local) user;
+  sopsEnabled = config.local.apps.sops.enable or false;
+in
 {
   nix = {
     optimise.automatic = true;
@@ -11,4 +18,11 @@
     nixfmt
     nix-output-monitor
   ];
+
+  sops.templates."user-nix-conf" = mkIf (user.enable && sopsEnabled) {
+    path = "/home/${user.name}/.config/nix/nix.conf";
+    owner = user.name;
+    mode = "0400";
+    content = "access-tokens = github.com=${config.sops.placeholder.github_token}\n";
+  };
 }
