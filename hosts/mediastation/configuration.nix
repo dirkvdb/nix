3f3bc8be6c1d3e4f8bf45f9f0ee51993c4a15320
Dirk Vanden Boer <dirk.vdb@gmail.com>
2026-04-20 10:17:55 +0200

    Update mediastation config

--- hosts/mediastation/configuration.nix
+++ hosts/mediastation/configuration.nix
@@ -76,7 +76,7 @@
           ethernet = {
             enable = true;
             wakeOnLan = true;
-            interface = "enp0s31f6";
+            interface = "en2ps0";
             dhcp = "ipv4";
           };
         };
@@ -84,11 +84,23 @@
         nfs-mounts = {
           enable = true;
           mounts = {
-            "/nas/media" = {
-              device = "nas.local:/volume1/media";
+            "/nas/secrets" = {
+              device = "nas.local:/volume2/secrets";
+            };
+            "/nas/ssd" = {
+              device = "nas.local:/volume2/ssd";
+            };
+            "/nas/downloads" = {
+              device = "nas.local:/volume1/downloads";
             };
             "/nas/data" = {
               device = "nas.local:/volume1/data";
+            };
+            "/nas/media" = {
+              device = "nas.local:/volume1/media";
+            };
+            "/nas/arr" = {
+              device = "nas.local:/volume1/arr";
             };
           };
         };
@@ -117,12 +129,8 @@
       };
 
       apps = {
-        direnv.enable = true;
         neovim.enable = true;
         sops.enable = true;
-        celluloid.enable = true;
-        vlc.enable = true;
-        localsend.enable = true;
       };
 
       home-manager = {
