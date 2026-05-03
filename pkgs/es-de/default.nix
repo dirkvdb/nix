bad51b121d5e08c519edb323226cd25e9f365d36
Dirk Vanden Boer <dirk.vdb@gmail.com>
2026-05-03 13:37:01 +0200

    es-de: Update to 3.4.1 and remove Eden patch

--- pkgs/es-de/default.nix
+++ pkgs/es-de/default.nix
@@ -5,7 +5,6 @@
   cmake,
   ninja,
   pkg-config,
-  xmlstarlet,
   gettext,
   ffmpeg,
   freetype,
@@ -25,19 +24,17 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "es-de";
-  version = "3.4.0";
+  version = "3.4.1";
 
   src = fetchurl {
     url = "https://gitlab.com/es-de/emulationstation-de/-/archive/v${finalAttrs.version}/emulationstation-de-v${finalAttrs.version}.tar.bz2";
-    hash = "sha256-S3nNtSimJlQAWyX62/nDlV58RWFcSqgQhHc7BU20d2U=";
+    hash = "sha256-dZJhY8hmLf4fwykvkF6U4lbaq1UqKE8ubf3FEx0rJXM=";
   };
 
   nativeBuildInputs = [
     cmake
     ninja
     pkg-config
-    gettext
-    xmlstarlet
   ];
 
   buildInputs = [
@@ -62,27 +59,6 @@ stdenv.mkDerivation (finalAttrs: {
   cmakeFlags = [
     "-DAPPLICATION_UPDATER=off"
   ];
-
-  postInstall = ''
-    systems_dir="$out/share/es-de/resources/systems/linux"
-    find_rules="$systems_dir/es_find_rules.xml"
-    systems_xml="$systems_dir/es_systems.xml"
-
-    # Append an EDEN emulator detection block for Linux.
-    sed -i 's|</ruleList>|    <emulator name="EDEN">\n        <!-- Nintendo Switch emulator Eden -->\n        <rule type="systempath">\n            <entry>eden-cli</entry>\n        </rule>\n        <rule type="staticpath">\n            <entry>~/Applications/*eden*.AppImage</entry>\n            <entry>~/.local/share/applications/*eden*.AppImage</entry>\n            <entry>~/.local/bin/*eden*.AppImage</entry>\n            <entry>~/bin/*eden*.AppImage</entry>\n            <entry>~/.local/bin/eden-cli</entry>\n            <entry>~/bin/eden-cli</entry>\n        </rule>\n    </emulator>\n</ruleList>|' \
-      "$find_rules"
-
-    # Add an EDEN launch command for the Nintendo Switch system.
-    # Use eden-cli with -f (fullscreen) and -g (game path).
-    xmlstarlet ed --inplace \
-      -s "/systemList/system[name='switch']" -t elem -n command -v "%EMULATOR_EDEN% -f -g %ROM%" \
-      "$systems_xml"
-
-    xmlstarlet ed --inplace \
-      -i "/systemList/system[name='switch']/command[text()='%EMULATOR_EDEN% -f -g %ROM%'][not(@label)]" \
-      -t attr -n label -v "Eden (Standalone)" \
-      "$systems_xml"
-  '';
 
   meta = with lib; {
     description = "ES-DE Frontend (EmulationStation Desktop Edition)";
