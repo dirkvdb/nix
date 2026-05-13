2710b2455dc9c5bfaeab39b7e015ee9aa339da1d
Dirk Vanden Boer <dirk.vdb@gmail.com>
2026-05-13 20:11:48 +0200

    Improved arr stack config

--- modules/nixos/services/nixflix/default.nix
+++ modules/nixos/services/nixflix/default.nix
@@ -16,6 +16,7 @@ in
     networking.firewall.allowedTCPPorts = [ 80 ];
 
     sops.secrets = {
+      "vpn/nordvpn.conf" = { };
       "sonarr/api_key" = { };
       "radarr/api_key" = { };
       "prowlarr/api_key" = { };
@@ -27,14 +28,30 @@ in
       "usenet/eweka/password" = { };
       "lidarr/api_key" = { };
       "seerr/api_key" = { };
+      "rutracker/username" = { };
+      "rutracker/password" = { };
+      "eztvl/username" = { };
+      "eztvl/password" = { };
     };
 
     nixflix = {
       enable = true;
+      theme = {
+        enable = true;
+        name = "nord";
+      };
+
+      vpn = {
+        enable = true;
+        wgConfFile = config.sops.secrets."vpn/nordvpn.conf".path;
+        accessibleFrom = [ "192.168.1.0/24" ];
+      };
 
       mediaDir = "/nas/media";
       downloadsDir = "/nas/downloads";
       mediaUsers = [ "dirk" ];
+
+      flaresolverr.enable = true;
 
       nginx = {
         enable = true;
@@ -75,7 +92,62 @@ in
         config = {
           apiKey._secret = config.sops.secrets."prowlarr/api_key".path;
           hostConfig.password._secret = config.sops.secrets."arr/password".path;
+          indexers = [
+            {
+              name = "RuTracker.org";
+              enable = true;
+              username._secret = config.sops.secrets."rutracker/username".path;
+              password._secret = config.sops.secrets."rutracker/password".path;
+              russianLetters = true;
+            }
+            {
+              name = "Generic Newznab";
+              enable = true;
+              redirect = true;
+              priority = 1;
+              baseUrl = "http://192.168.1.13:8383/newznab";
+            }
+            {
+              name = "BT.etree";
+              enable = true;
+              baseUrl = "https://bt.etree.org/";
+            }
+            {
+              name = "The Pirate Bay";
+              enable = true;
+              baseUrl = "https://thepiratebay.org/";
+            }
+            {
+              name = "MixtapeTorrent";
+              enable = true;
+              baseUrl = "http://www.mixtapetorrent.com/";
+            }
+            # {
+            #   name = "EZTV";
+            #   enable = true;
+            #   baseUrl = "https://eztvx.to/";
+            #   tags = [ "flaresolverr" ];
+            # }
+            # {
+            #   name = "EZTVL";
+            #   enable = true;
+            #   baseUrl = "https://eztvx.to/";
+            #   tags = [ "flaresolverr" ];
+            #   username._secret = config.sops.secrets."eztvl/username".path;
+            #   password._secret = config.sops.secrets."eztvl/password".path;
+            # }
+          ];
         };
+      };
+
+      torrentClients.qbittorrent = {
+        enable = true;
+        password._secret = config.sops.secrets."arr/password".path;
+        serverConfig.Preferences.WebUI.Username = config.local.user.name;
+        serverConfig.Preferences.WebUI.Password_PBKDF2 =
+          ''@ByteArray(GOT/oxX4EohRRMf1FOoGAA==:nNroSP4qegzAps3jIK/qQsWAXuC/F7slljkkN4UdVbbFO6/O7QYPhi36ZBqqE/k8Ra9BWFCaxiLs0Yq5XqjFhg==)'';
+        serverConfig.Preferences.WebUI.AuthSubnetWhitelistEnabled = true;
+        serverConfig.Preferences.WebUI.AuthSubnetWhitelist = "192.168.1.0/24";
       };
 
       usenetClients.sabnzbd = {
