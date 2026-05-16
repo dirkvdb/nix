00734260e06506bd926c0bf87c7ed2cf44ce2d77
Dirk Vanden Boer <dirk.vdb@gmail.com>
2026-05-16 18:54:35 +0200

    Add supportedLocales for en_US, nl_BE, and nl_NL

--- modules/nixos/system/locale/default.nix
+++ modules/nixos/system/locale/default.nix
@@ -4,6 +4,11 @@
 {
   i18n = {
     defaultLocale = "en_US.UTF-8";
+    supportedLocales = [
+      "en_US.UTF-8/UTF-8"
+      "nl_BE.UTF-8/UTF-8"
+      "nl_NL.UTF-8/UTF-8"
+    ];
     extraLocaleSettings = {
       LC_ADDRESS = "nl_BE.UTF-8";
       LC_IDENTIFICATION = "nl_BE.UTF-8";
