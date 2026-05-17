1e79dc52d87ccd8b6d19b9a550331f7d0f944d2a
Dirk Vanden Boer <dirk.vdb@gmail.com>
2026-05-17 20:41:19 +0200

    Switch kiosk desktop to use cage instead of gamescope

--- modules/nixos/desktop/kiosk/default.nix
+++ modules/nixos/desktop/kiosk/default.nix
@@ -1,18 +1,16 @@
 {
   lib,
   pkgs,
+  unstablePkgs,
   config,
   ...
 }:
 let
   cfg = config.local.desktop.kiosk;
-  gamescopeArgs = lib.concatStringsSep " " cfg.gamescopeArgs;
-  steamArgs = lib.concatStringsSep " " cfg.steamArgs;
-  command = "${lib.getExe pkgs.gamescope} ${gamescopeArgs} -- steam ${steamArgs} > /dev/null 2>&1";
 in
 {
   options.local.desktop.kiosk = {
-    enable = lib.mkEnableOption "Enable kiosk/TV desktop environment using gamescope";
+    enable = lib.mkEnableOption "Enable kiosk desktop environment using cage";
 
     user = lib.mkOption {
       type = lib.types.str;
@@ -20,44 +18,41 @@ in
       description = "The user to run the kiosk session as.";
     };
 
-    gamescopeArgs = lib.mkOption {
-      type = lib.types.listOf lib.types.str;
-      default = [
-        "-W 1920"
-        "-H 1080"
-        "-f"
-        "-e"
-        "--xwayland-count 2"
-      ];
-      description = "Arguments passed to gamescope.";
+    program = lib.mkOption {
+      type = lib.types.path;
+      default = lib.getExe pkgs.es-de;
+      description = "The program to run inside the cage kiosk session.";
     };
 
-    steamArgs = lib.mkOption {
+    extraArgs = lib.mkOption {
       type = lib.types.listOf lib.types.str;
-      default = [
-        "-pipewire-dmabuf"
-        "-gamepadui"
-      ];
-      description = "Arguments passed to Steam inside gamescope.";
+      # -d: don't draw client-side decorations (cleaner kiosk look)
+      default = [ "-d" ];
+      example = [ "-m last" ];
+      description = "Extra arguments passed to cage.";
+    };
+
+    environment = lib.mkOption {
+      type = lib.types.attrsOf lib.types.str;
+      default = {
+        # Don't error if no input devices are detected at startup
+        WLR_LIBINPUT_NO_DEVICES = "1";
+      };
+      description = "Additional environment variables for the cage session.";
     };
   };
 
   config = lib.mkIf cfg.enable {
-    programs.gamescope = {
+    services.cage = {
       enable = true;
-      capSysNice = true;
+      user = cfg.user;
+      program = cfg.program;
+      extraArguments = cfg.extraArgs;
+      environment = cfg.environment;
+      # Use cage from unstable; may have a fix for XWayland/bwrap issues.
+      package = unstablePkgs.cage;
     };
 
-    programs.steam.gamescopeSession.enable = true;
-
-    services.greetd = {
-      enable = true;
-      settings = {
-        default_session = {
-          inherit command;
-          user = cfg.user;
-        };
-      };
-    };
+    environment.systemPackages = [ pkgs.es-de ];
   };
 }
