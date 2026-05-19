599f8a647c0c92f86973d1bbff2a2a44b6b1a196
Dirk Vanden Boer <dirk.vdb@gmail.com>
2026-05-19 17:19:44 +0200

    lemonade: Bump to 10.5.1 and update patch logic

--- pkgs/lemonade/version.nix
+++ pkgs/lemonade/version.nix
@@ -1,8 +1,8 @@
 {
-  version = "10.4.0";
+  version = "10.5.1";
   src = {
     owner = "lemonade-sdk";
     repo = "lemonade";
-    hash = "sha256-NQmNdpeC38k0RdGs4WeYnWRomsr0/r98GeDcgt5TzNs=";
+    hash = "sha256-jPc/KeuDUrkGSfvDx4rWB9tL6+f6w2Le6zMNvbj4y7Q=";
   };
 }
