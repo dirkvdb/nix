11f00b04a8abf029a5d905085c4826b1c11ae471
Dirk Vanden Boer <dirk.vdb@gmail.com>
2026-05-22 07:20:28 +0200

    Bump lemonade to 10.6.0 and update source hash

--- pkgs/lemonade/version.nix
+++ pkgs/lemonade/version.nix
@@ -1,8 +1,8 @@
 {
-  version = "10.5.1";
+  version = "10.6.0";
   src = {
     owner = "lemonade-sdk";
     repo = "lemonade";
-    hash = "sha256-jPc/KeuDUrkGSfvDx4rWB9tL6+f6w2Le6zMNvbj4y7Q=";
+    hash = "sha256-VxYDmfcdi7YaELAC5Pp1dzr7uhtWHM2YUSCnnNO8aV8=";
   };
 }
