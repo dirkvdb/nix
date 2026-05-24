acdf00cf6c6e21503e309ff50ac5e476c2cef173
Dirk Vanden Boer <dirk.vdb@gmail.com>
2026-05-24 14:32:12 +0200

    Add hyprmoncfg home-manager module

---
+++ modules/home/services/hyprmoncfg/default.nix
@@ -0,0 +1,26 @@
+{
+  lib,
+  pkgs,
+  config,
+  mkHome,
+  ...
+}:
+let
+  inherit (config.local) user;
+  cfg = config.local.services.hyprmoncfg;
+  mkUserHome = mkHome user.name;
+in
+{
+  options.local.services.hyprmoncfg = {
+    enable = lib.mkEnableOption "hyprmoncfg monitor profile daemon";
+  };
+
+  config = lib.mkIf cfg.enable (mkUserHome {
+    home.packages = [ pkgs.hyprmoncfg ];
+
+    xdg.configFile."systemd/user/hyprmoncfgd.service".source =
+      "${pkgs.hyprmoncfg}/share/systemd/user/hyprmoncfgd.service";
+    xdg.configFile."systemd/user/default.target.wants/hyprmoncfgd.service".source =
+      "${pkgs.hyprmoncfg}/share/systemd/user/hyprmoncfgd.service";
+  });
+}
