d4a6fca3d418b4f874642e36c3386a231804bafa
Dirk Vanden Boer <dirk.vdb@gmail.com>
2026-05-24 14:32:12 +0200

    Add hyprmoncfg home-manager module

--- pkgs/hyprmoncfg/default.nix
+++ pkgs/hyprmoncfg/default.nix
@@ -20,6 +20,15 @@
 
   doCheck = false;
 
+  postInstall = ''
+    install -Dm644 packaging/systemd/hyprmoncfgd.service $out/lib/systemd/user/hyprmoncfgd.service
+    substituteInPlace $out/lib/systemd/user/hyprmoncfgd.service \
+      --replace-fail '/usr/bin/hyprmoncfgd' "$out/bin/hyprmoncfgd"
+
+    install -Dm644 packaging/applications/hyprmoncfg.desktop $out/share/applications/hyprmoncfg.desktop
+    install -Dm644 packaging/icons/hyprmoncfg.svg $out/share/icons/hicolor/scalable/apps/hyprmoncfg.svg
+  '';
+
   meta = {
     description = "Arrange Hyprland monitors without coordinate math";
     homepage = "https://github.com/crmne/hyprmoncfg";
