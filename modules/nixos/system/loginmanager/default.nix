a8bfc949c27ade9e001cd94da05977b8b75708e5
Dirk Vanden Boer <dirk.vdb@gmail.com>
2026-05-24 20:37:09 +0200

    Fix deprecation warning

--- modules/nixos/system/loginmanager/default.nix
+++ modules/nixos/system/loginmanager/default.nix
@@ -3,12 +3,11 @@
 }:
 {
   services.logind = {
-    lidSwitch = "suspend";
-    lidSwitchDocked = "ignore";
-    lidSwitchExternalPower = "suspend";
-
     # make sure the power button does not suspend or shutdown the system, it is handled by hyprland
     settings.Login = {
+      HandleLidSwitch = "suspend";
+      HandleLidSwitchDocked = "ignore";
+      HandleLidSwitchExternalPower = "suspend";
       HandlePowerKey = "ignore";
       #HandlePowerKeyLongPress = "ignore";
       PowerKeyIgnoreInhibited = "yes";
