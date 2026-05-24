a9286d9e6cd954268cb787557908868362573b62
Dirk Vanden Boer <dirk.vdb@gmail.com>
2026-05-24 14:32:31 +0200

    Set logind lid switch actions and refactor power key config

--- modules/nixos/system/loginmanager/default.nix
+++ modules/nixos/system/loginmanager/default.nix
@@ -2,10 +2,16 @@
   ...
 }:
 {
-  # make sure the power button does not suspend or shutdown the system, it is handled by hyprland
-  services.logind.settings.Login = {
-    HandlePowerKey = "ignore";
-    #HandlePowerKeyLongPress = "ignore";
-    PowerKeyIgnoreInhibited = "yes";
+  services.logind = {
+    lidSwitch = "suspend";
+    lidSwitchDocked = "ignore";
+    lidSwitchExternalPower = "suspend";
+
+    # make sure the power button does not suspend or shutdown the system, it is handled by hyprland
+    settings.Login = {
+      HandlePowerKey = "ignore";
+      #HandlePowerKeyLongPress = "ignore";
+      PowerKeyIgnoreInhibited = "yes";
+    };
   };
 }
