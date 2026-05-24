87d80ea58d80c64d8ac42dea9af495662866bffc
Dirk Vanden Boer <dirk.vdb@gmail.com>
2026-05-24 20:35:45 +0200

    Remove lemonade package set from pkgs

--- pkgs/lemonade/default.nix
+++
@@ -1,9 +0,0 @@
-{ callPackage }:
-let
-  lemonade-web-app-bundle = callPackage ./web-app-bundle.nix { };
-in
-{
-  lemonade-server = callPackage ./server.nix { inherit lemonade-web-app-bundle; };
-  lemonade-app = callPackage ./app.nix { };
-  inherit lemonade-web-app-bundle;
-}
