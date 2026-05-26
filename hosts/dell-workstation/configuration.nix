61eab7a31c512f390c795e22290abbd93aebb64c
Dirk Vanden Boer <dirk.vdb@gmail.com>
2026-05-26 16:34:01 +0200

    Add cuda binary cache

--- hosts/dell-workstation/configuration.nix
+++ hosts/dell-workstation/configuration.nix
@@ -31,6 +31,15 @@
       enable = true;
     };
 
+    nix.settings = {
+      substituters = [
+        "https://cache.nixos-cuda.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
+      ];
+    };
+
     boot.kernelPackages = pkgs.linuxPackages_latest;
 
     # NVIDIA + Intel PRIME configuration for the Precision 7670 hybrid graphics
