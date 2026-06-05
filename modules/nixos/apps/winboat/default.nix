d3e2ee294087cd4467c693c37aad3b5ffe67baaa
Dirk Vanden Boer <dirk.vdb@gmail.com>
2026-06-05 10:59:37 +0200

    Disable WinBoat container autostart on boot

--- modules/nixos/apps/winboat/default.nix
+++ modules/nixos/apps/winboat/default.nix
@@ -1,5 +1,6 @@
 {
   lib,
+  pkgs,
   unstablePkgs,
   config,
   ...
@@ -16,5 +17,20 @@ in
     environment.systemPackages = [
       unstablePkgs.winboat
     ];
+
+    # Prevent the WinBoat Docker container from starting automatically at boot.
+    # WinBoat sets a restart policy of "on-failure" by default; override it to
+    # "no" so the container only runs when explicitly started via the app.
+    systemd.services.winboat-no-autostart = {
+      description = "Disable WinBoat Docker container autostart";
+      after = [ "docker.service" ];
+      requires = [ "docker.service" ];
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit = true;
+        ExecStart = "${pkgs.docker}/bin/docker update --restart=no WinBoat";
+      };
+    };
   };
 }
