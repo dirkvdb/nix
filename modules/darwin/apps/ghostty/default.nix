5996bac41ef161049ca3823bb13aa641fe4db0b9
Dirk Vanden Boer <dirk.vdb@gmail.com>
2026-06-08 07:12:06 +0200

    Move Ghostty option definition to shared module

--- modules/darwin/apps/ghostty/default.nix
+++ modules/darwin/apps/ghostty/default.nix
@@ -7,10 +7,6 @@ let
   cfg = config.local.apps.ghostty;
 in
 {
-  options.local.apps.ghostty = {
-    enable = lib.mkEnableOption "Ghostty";
-  };
-
   config = lib.mkIf cfg.enable {
     homebrew.casks = [
       "ghostty"
