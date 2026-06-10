f8a1b3ab9c57140c8f8d02c47d8d1ef0c1e619cc
Dirk Vanden Boer <dirk.vdb@gmail.com>
2026-06-10 11:17:13 +0200

    Force Spotify to use configured display scale

--- modules/nixos/apps/spotify/default.nix
+++ modules/nixos/apps/spotify/default.nix
@@ -6,14 +6,20 @@
 }:
 let
   cfg = config.local.apps.spotify;
+  scale = toString config.local.desktop.displayScale;
 in
 {
   options.local.apps.spotify = {
     enable = lib.mkEnableOption "Install Spotify desktop app";
   };
   config = lib.mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [
-      spotify
+    environment.systemPackages = [
+      (pkgs.spotify.overrideAttrs (old: {
+        postInstall = (old.postInstall or "") + ''
+          wrapProgram $out/bin/spotify \
+            --add-flags "--force-device-scale-factor=${scale}"
+        '';
+      }))
     ];
   };
 }
