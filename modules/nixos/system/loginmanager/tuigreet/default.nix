a971103f3b78e4aecff7fa85bc1822abfb1bb4f4
Dirk Vanden Boer <dirk.vdb@gmail.com>
2026-06-22 10:00:46 +0200

    Add tuigreet autologin options

--- modules/nixos/system/loginmanager/tuigreet/default.nix
+++ modules/nixos/system/loginmanager/tuigreet/default.nix
@@ -10,13 +10,31 @@ in
 {
   options.local.system.loginmanager.tuigreet = {
     enable = lib.mkEnableOption "Enable the tuigreet login manager";
+
+    autologin = {
+      enable = lib.mkEnableOption "Auto-login on first boot (tuigreet as fallback on session exit)";
+      user = lib.mkOption {
+        type = lib.types.str;
+        description = "The username to auto-login as";
+      };
+      command = lib.mkOption {
+        type = lib.types.str;
+        default = "uwsm start hyprland-uwsm.desktop";
+        description = "The session command for the initial auto-login";
+      };
+    };
   };
 
   config = lib.mkIf cfg.enable {
-    services = {
-      greetd = {
-        enable = true;
-        settings.default_session.command = "${pkgs.tuigreet}/bin/tuigreet --asterisks --remember --remember-session --time --sessions /run/current-system/sw/share/wayland-sessions --session-wrapper 'systemd-cat -t session'";
+    services.greetd = {
+      enable = true;
+      restart = !cfg.autologin.enable;
+      settings = {
+        default_session.command = "${pkgs.tuigreet}/bin/tuigreet --asterisks --remember --remember-session --time --sessions /run/current-system/sw/share/wayland-sessions --session-wrapper 'systemd-cat -t session'";
+        initial_session = lib.mkIf cfg.autologin.enable {
+          command = cfg.autologin.command;
+          user = cfg.autologin.user;
+        };
       };
     };
   };
