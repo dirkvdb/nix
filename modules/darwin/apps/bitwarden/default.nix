32e48678ef654a93f00c26e008835c6d154e3d29
Dirk Vanden Boer <dirk.vdb@gmail.com>
2026-08-10 23:14:03 +0200

    Fix darwin config

--- modules/darwin/apps/bitwarden/default.nix
+++ modules/darwin/apps/bitwarden/default.nix
@@ -7,10 +7,6 @@ let
   cfg = config.local.apps.bitwarden;
 in
 {
-  options.local.apps.bitwarden = {
-    enable = lib.mkEnableOption "Install Bitwarden desktop app";
-  };
-
   config = lib.mkIf cfg.enable {
     homebrew = {
       casks = [
