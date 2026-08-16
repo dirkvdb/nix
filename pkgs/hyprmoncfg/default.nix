62f36783930e89b8d59a42a803708f9cf5e636b1
Dirk Vanden Boer <dirk.vdb@gmail.com>
2026-08-16 16:37:45 +0200

    Bump hyprmoncfg to 1.13.0

--- pkgs/hyprmoncfg/default.nix
+++ pkgs/hyprmoncfg/default.nix
@@ -8,13 +8,13 @@
 
 (buildGoModule.override { go = go_1_26; }) (finalAttrs: {
   pname = "hyprmoncfg";
-  version = "1.9.1";
+  version = "1.13.0";
 
   src = fetchFromGitHub {
     owner = "crmne";
     repo = "hyprmoncfg";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-LCZ1F30Ix4NnWYPI3WkL03jke9XW1cB5CWxmVSrdYYI=";
+    hash = "sha256-R1o1y16Muq0RNUk5AWfTgBFDCNLvm2/m8AWSfjsWx4k=";
   };
 
   vendorHash = "sha256-gQbjvdKtO0hCXrs9RnWo1s0YeHf5W9t+8AgS2ELXlPo=";
