6e02df82bc83c677782fc371bc675a511e8761d8
Dirk Vanden Boer <dirk.vdb@gmail.com>
2026-08-20 21:47:15 +0200

    Bump noctalia input to v5.0.0-beta.9

--- flake.nix
+++ flake.nix
@@ -93,7 +93,7 @@
     };
 
     noctalia = {
-      url = "github:noctalia-dev/noctalia/v5.0.0-beta.8";
+      url = "github:noctalia-dev/noctalia/v5.0.0-beta.9";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
