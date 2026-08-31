d8ec65a72ae8fbdc255f796a4540df72bca84664
Dirk Vanden Boer <dirk.vdb@gmail.com>
2026-08-31 10:12:20 +0200

    Remove obsolete lockscreen widget configuration

--- modules/nixos/desktop/noctalia/default.nix
+++ modules/nixos/desktop/noctalia/default.nix
@@ -165,33 +165,6 @@ let
 
     lockscreen_widgets = {
       enabled = false;
-      schema_version = 2;
-      widget_order = [ "lockscreen-login-box@eDP-1" ];
-      grid = {
-        cell_size = 16;
-        major_interval = 4;
-        visible = true;
-      };
-      widget."lockscreen-login-box@eDP-1" = {
-        box_height = 70.0;
-        box_width = 400.0;
-        cx = 756.0;
-        cy = 863.0;
-        output = "eDP-1";
-        rotation = 0.0;
-        type = "login_box";
-        settings = {
-          background_color = "surface_variant";
-          background_opacity = 0.88;
-          background_radius = 12.0;
-          center_password_text = false;
-          input_opacity = 1.0;
-          input_radius = 6.0;
-          show_caps_lock = true;
-          show_keyboard_layout = true;
-          show_login_button = true;
-        };
-      };
     };
 
     nightlight = {
