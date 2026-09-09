d7f82f03565d927694c496313cd36c6947790333
Dirk Vanden Boer <dirk.vdb@gmail.com>
2026-09-09 08:19:17 +0200

    Remove Numtide cache settings from flake.nix

--- flake.nix
+++ flake.nix
@@ -1,13 +1,6 @@
 {
   description = "nix system flake";
 
-  nixConfig = {
-    extra-substituters = [ "https://cache.numtide.com" ];
-    extra-trusted-public-keys = [
-      "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
-    ];
-  };
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
