0ec810949c5a8c63bb2ee7f5edf3545878b202b1
Dirk Vanden Boer <dirk.vdb@gmail.com>
2026-09-12 10:59:18 +0200

    Bump ai-usagebar input to v1.16.0

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
     };
 
     ai-usagebar = {
-      url = "github:akitaonrails/ai-usagebar/v1.15.0";
+      url = "github:akitaonrails/ai-usagebar/v1.16.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
