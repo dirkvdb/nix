e41828c144b159838c91ac22d3c9d2984701a9da
Dirk Vanden Boer <dirk.vdb@gmail.com>
2026-09-16 00:04:53 +0200

    Set systemd memlock limits to infinity for Fastflowlm support

--- modules/nixos/system/default.nix
+++ modules/nixos/system/default.nix
@@ -5,10 +5,12 @@
   systemd.settings.Manager = {
     DefaultTimeoutStopSec = "10s";
     DefaultTimeoutAbortSec = "10s"; # optional
+    DefaultLimitMEMLOCK = "infinity";
   };
 
   systemd.user.extraConfig = ''
     DefaultTimeoutStopSec=10s
+    DefaultLimitMEMLOCK=infinity
   '';
 
   documentation.man.cache.enable = false;
