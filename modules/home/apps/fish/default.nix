8a6f4eea260f370aa1bbadc39ff61375c1bff954
Dirk Vanden Boer <dirk.vdb@gmail.com>
2026-09-24 21:21:41 +0200

    Push the active system path with xilo

--- modules/home/apps/fish/default.nix
+++ modules/home/apps/fish/default.nix
@@ -15,7 +15,7 @@ let
   hostname = config.local.system.network.hostname or "";
   cachePush = lib.optionalString (
     hostname == "mini" || nhConfigurationName == "dell-workstation"
-  ) " && xilo push admin/nix ~/nix/result";
+  ) " && xilo push admin/nix /run/current-system";
   mkUserHome = mkHome user.name;
 in
 {
